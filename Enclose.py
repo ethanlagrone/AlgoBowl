f35551389abd9cb51b1e98916d568df4d2df9d0f
ethanlagrone <ethan_lagrone@mines.edu>
2026-04-04 12:39:41 -0600

    sorta functional enclose.py that isnt very good lol

--- Enclose.py
+++ Enclose.py
@@ -11,16 +11,25 @@ def optimize(maze):
     #recursively try to optimize the maze(ie. push walls back one and ensure horse cannot escape)
     return 0
 
-def recursiveEncloseHorse(maze, wallCount, portalPairCoords):
-    #recursively create a maze until some base case
-    return 0
+def recursiveEncloseHorse(G, maze, walls, grassNodes):
+    #Source: https://stackoverflow.com/questions/44532952/find-number-of-connected-edges-to-a-node-and-node-with-max-connected-edges 
+    G.degree()
+    leastConnected = min(grassNodes, key=lambda n: G.degree[n])
+    node = leastConnected 
+    element,i,j, value = node
+    if(maze[i][j] == '.'):
+        maze[i][j] = 'W'
+        walls += 1
+        grassNodes.remove(node)
+        return maze, walls, grassNodes
 
 def encloseHorse(maze, wallCount, portalPairCoords):
     changable = ['.', 'W']
     changableNodes = []
     outsideHoles = []
     outsideHoleCount = 0
-    currentNumWalls = 0
+    walls = 0
+    grassNodes = []
 
     #Find all the nodes that we can change to trap the horse 
     #also count how many holes are on the outside
@@ -48,8 +57,27 @@ def encloseHorse(maze, wallCount, portalPairCoords):
             return maze
     else:
         #trap horse, then recursively expand
-        print("Not handled yet")
-        exit(1)
+        for i in range(len(maze)):
+            for j in range(len(maze[i])):
+                current = maze[i][j]
+                if(current == 'W'):
+                    maze[i][j] = '.'
+                if(current == 'H'):
+                    horseCoords = (i,j)  
+                if(current == '.'):
+                    grassNodes.append(('.',i,j,1))  
+        
+        G = CreateGraph.createGraph(wallCount, maze, portalPairCoords)
+        while(True):
+            maze, walls, grassNodes = recursiveEncloseHorse(G, maze, walls, grassNodes)
+            G = CreateGraph.createGraph(wallCount, maze, portalPairCoords)
+            if(not Validater.horseCanEscape(G)):
+                return maze 
+            if(walls >= wallCount):
+                print("Too many walls brother")
+                return maze
+
+
             
 
 
