fcf3954afa513bdeaba4c4fa08b27315c46a0193
Morgan Smith <msmith120304@gmail.com>
2026-04-05 23:33:43 -0600

    half of the input thing working
    i'm writing some bullshittttt

--- TeamInputGenerator.py
+++ TeamInputGenerator.py
@@ -1,13 +1,28 @@
 board_size = 97
+center = int(board_size / 2)
 grid = [['.' for _ in range(board_size)] for _ in range(board_size)]
+
 steps_to_center = 0.25 * board_size + 0.75
-stop = int(board_size / 2) - int(steps_to_center) + 1
+stop_top = center - int(steps_to_center) + 1
 
-for i in range(1, stop):
-    grid[i][int(board_size / 2) - (i - 1)] = 'b'
-    grid[i][int(board_size / 2) + (i - 1)] = 'b'
+side_row_count = 2 * (stop_top - 1) - 1
+start_sides = stop_top + 2
+end_sides = start_sides + side_row_count
 
-grid[int(board_size / 2)][int(board_size / 2)] = 'H'
+start_bottom = center + int(steps_to_center)
+
+
+for i in range(1, stop_top):
+    grid[i][center - (i - 1)] = 'b'
+    grid[i][center + (i - 1)] = 'b'
+
+for i in range(start_sides, center):
+    grid[i][center - i + 1] = 'b'
+    grid[i][board_size - 1 - (i - 2 * (i - start_sides)) - 1 + 4] = 'b'     # yeah idk either
+
+grid[center][1] = 'b'
+grid[center][board_size - 2] = 'b'
+grid[center][center] = 'H'
 
 with open("TeamInput.txt", "w") as f:
     for row in grid:
