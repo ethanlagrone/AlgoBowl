881329ae77aa50d8c94cdc01d94ef93bb16ba9e5
Morgan Smith <msmith120304@gmail.com>
2026-04-05 22:51:33 -0600

    like a quarter of the input is working

--- TeamInputGenerator.py
+++ TeamInputGenerator.py
@@ -1,11 +1,13 @@
 board_size = 97
 grid = [['.' for _ in range(board_size)] for _ in range(board_size)]
+steps_to_center = 0.25 * board_size + 0.75
+stop = int(board_size / 2) - int(steps_to_center) + 1
 
-for i in range(1, board_size - 1):
-    grid[i][(board_size / 2) - (i - 1)] = 'b'
-    grid[i][(board_size / 2) + (i - 1)] = 'b'
+for i in range(1, stop):
+    grid[i][int(board_size / 2) - (i - 1)] = 'b'
+    grid[i][int(board_size / 2) + (i - 1)] = 'b'
 
-grid[board_size / 2][board_size / 2] = 'H'
+grid[int(board_size / 2)][int(board_size / 2)] = 'H'
 
 with open("TeamInput.txt", "w") as f:
     for row in grid:
